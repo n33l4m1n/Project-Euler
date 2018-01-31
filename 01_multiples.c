167135ea22f3c45d7dd8315bcb16cf6d85d1db76
n33l4m1n <neelamin27@gmail.com>
2018-01-30 20:42:29 -0500

    beep

--- 01_multiples.c
+++ 01_multiples.c
@@ -1,27 +1,23 @@
-// Neelkumar Amin
+// n33l4m1n
 // Project Euler
 // September 29th, 2017
-
-// ==============
-// Multiples of 3 and 5: program01.c
-// ==============
-
+// ==========================================================
+// Multiples of 3 and 5
 // If we list all the natural numbers below 10 that
 // are multiples of 3 or 5, we get 3, 5, 6 and 9.
 // The sum of these multiples is 23.
-//
 // Find the sum of all the multiples of 3 or 5 below 1000.
+// ===========================================================
 
 #include <stdio.h>
 
-#define LIMIT 1000
 
 int multiples(int n);
 
 int main(void)
 {
-  printf("%d\n", multiples(LIMIT));
-
+  int n = 1000; // 1000
+  printf("%d\n", multiples(n));
   return 0;
 }
 
@@ -29,27 +25,12 @@ int multiples(int n)
 {
   int i;
   int sum = 0;
-  // int threes = 0;
-  // int fives = 0;
-
+  
   for(i = 0; i < n; i++)
   {
-    // This method gives the wrong answer.
-    // The numbers that are multiples of both
-    // three and five are counted twice for
-    // each loop through; 266333 is the wrong sum.
-    //
-    // if (i % 3 == 0)
-    //   threes = threes + i;
-    // if (i % 5 == 0)
-    //   fives = fives + i;
-    //
-    // sum = threes + fives;
-
     if(i % 3 == 0 || i % 5 == 0)
       sum = sum + i;
-    }
-
+  }
     return sum;
 }
 
