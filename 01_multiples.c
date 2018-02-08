2ad704792caa125aa26aea656261b6233fd4ef53
n33l4m1n <neelamin27@gmail.com>
2018-02-08 18:06:55 -0500

    multiples

--- 01_multiples.c
+++ 01_multiples.c
@@ -7,10 +7,9 @@
 // are multiples of 3 or 5, we get 3, 5, 6 and 9.
 // The sum of these multiples is 23.
 // Find the sum of all the multiples of 3 or 5 below 1000.
-// ===========================================================
+// ==========================================================
 
 #include <stdio.h>
-
 
 int multiples(int n);
 
